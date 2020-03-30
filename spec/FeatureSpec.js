3c70a80f7c0211dce183d53c91f8f83f071f450f
Ben Galley <bengalley21@gmail.com>
2020-03-30 12:20:10 +0100

    first commit, first feature test

---
+++ spec/FeatureSpec.js
@@ -0,0 +1,17 @@
+'use strict'
+
+describe("Feature Spec", function(){
+  var game;
+  var frame;
+
+  beforeEach(function(){
+    game = new Game();
+    frame = new Frame();
+  });
+
+  it("adds the frame score to the game score", function(){
+    frame.roll(9)
+    game.add(frame.score)
+    expect(game.score).toEqual(9)
+  });
+});
